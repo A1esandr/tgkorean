f5aa3844b7864d7636b3d47c06b1ccc8ec898194
A1esandr <a1exswan001@gmail.com>
2021-11-03 00:12:58 +0500

    added app stub

--- tgkorean.go
+++ tgkorean.go
@@ -1,1 +1,21 @@
 package tgkorean
+
+type (
+	app struct {
+	}
+	App interface {
+		Start()
+	}
+	AppParams struct {
+		Token  string
+		ChatID int64
+	}
+)
+
+func New(params AppParams) App {
+	return &app{}
+}
+
+func (a *app) Start() {
+
+}
